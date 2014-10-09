368f5a31a7f2c71a7a4d0571e217881ff60a979b
Margo73465 <margo.lee.smith@gmail.com>
2014-10-09 15:33:14 -0400

    fixed height/width bug, added death checks and borders

--- snake.py
+++ snake.py
@@ -4,6 +4,7 @@ from curtsies.fmtfuncs import red, bold, green, on_blue, yellow, on_red, blue
 
 MAX_FPS = 10
 time_per_frame = 1. / MAX_FPS
+#isDead = False
 
 class FrameCounter(object):
     def __init__(self):
@@ -23,25 +24,37 @@ class SnakeGame(object):
         self.width = width
         self.height = height
         self.direction = (1, 0)
-    def render(self, isDead):
-        if not isDead:
-            a = FSArray(self.height, self.width)
+        self.isDead = False
+    def render(self):
+        a = FSArray(self.height, self.width)
+        # Border
+        for i in range(0, self.width):
+            a[i, 0] = bold('*')
+            a[i, self.height] = bold('*')
+        for j in range(0, self.height):
+            a[0, j] = bold('*')
+            a[self.width, j] = bold('*')
+        # If not dead, add snake to screen
+        if not self.isDead:
             for seg in self.snake_segments:
                 a[seg[0],seg[1]] = blue('X')
             return a
         else:
-            a = self.deathSequence()
+            a = self.deathSequence(a)
             return a
-
     def move(self):
-        self.snake_segments.insert(0, (self.snake_segments[0][0] + self.direction[0],
-                                      (self.snake_segments[0][1] + self.direction[1])))
-        self.snake_segments.pop(3)
-
-    def deathSequence(self):
-        a = FSArray(self.height, self.width)
-        a[10,10] = red('X')
-        a[10,14] = red('X')
+    	new_x = self.snake_segments[0][0] + self.direction[0]
+    	new_y = self.snake_segments[0][1] + self.direction[1]
+    	if new_x == 0 or new_x == self.width or new_y == 0 or new_y == self.height:
+    		self.isDead = True
+        elif (new_x, new_y) in self.snake_segments:
+            self.isDead = True
+    	else:
+        	self.snake_segments.insert(0, (new_x, new_y))
+        	self.snake_segments.pop(3)
+    def deathSequence(self, a):
+        a[10, 10] = red('X')
+        a[10, 14] = red('X')
         a[12, 10] = red('_')
         a[12, 11] = red('_')
         a[12, 12] = red('_')
@@ -52,8 +65,9 @@ class SnakeGame(object):
 def main():
     counter = FrameCounter()
     with FullscreenWindow() as window:
+    	# Why don't we see this? 
         print('Press escape to exit')
-        game = SnakeGame(window.height, window.width)
+        game = SnakeGame(window.width, window.height)
         with Input() as input_generator:
             c = None
             last_c = '<DOWN>'
@@ -61,6 +75,8 @@ def main():
                 t0 = time.time()
                 while True:
                     t = time.time()
+                    # This tells you how long you're willing to wait before you just move the snake along
+                    # equal to time_per_frame
                     temp_c = input_generator.send(max(0, t - (t0 + time_per_frame)))                    
                     if temp_c is not None:
                         c = temp_c
@@ -84,13 +100,13 @@ def main():
                     if time_per_frame < t - t0:
                         break
 
-                fps = 'FPS: %.1f' % counter.fps()
-                a = game.render(isDead = False) # insert death boolean
-                a[0:1, 0:len(fps)] = [fps]
+                #fps = 'FPS: %.1f' % counter.fps()
+                #a[0:1, 0:len(fps)] = [fps]
 
                 game.move()
+                a = game.render() # insert death boolean
                 window.render_to_terminal(a)
-                counter.frame()
+                #counter.frame()
                 
 
 if __name__ == '__main__':
